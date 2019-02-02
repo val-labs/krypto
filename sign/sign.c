ddd97025c99ec7fbe9592a7b2802abaecaa4a209
Joel Ward <jmward@gmail.com>
2019-02-01 20:18:54 -0800

    update

--- sign/sign.c
+++ sign/sign.c
@@ -9,14 +9,7 @@
 #define  WRN(fmt,...) ((fprintf(stderr, "%s:%d:ERR: " fmt   "", __FILE__, __LINE__, ##__VA_ARGS__)))
 #define  DIE(fmt,...) ((fprintf(stderr, "%s:%d:DIE: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__),exit(1)))
 #define PERR(fmt,...) ((fprintf(stderr, "%s:%d:ERR: " fmt   "", __FILE__, __LINE__, ##__VA_ARGS__),perror(""),exit(1)))
-#define DUMP0(s,m)				\
-    for(int n=0;n<m;n++)			\
-      printf("%02x", (s)[n]),			\
-	printf((n+1)%32?" ":"\n");
-#define DUMP(s,m)				\
-    for(int n=0;n<m;n++)			\
-      printf("%02x", (s)[n]),			\
-	printf((n+1)%32?"":"\n");
+#define DUMP(s,m) for(int n=0;n<m;n++)printf("%02x%s",(s)[n],(n+1)%32?"":"\n");
 #define default break;default
 #define case    break;case
 unsigned char* read_seed(char* filename, unsigned char*buf) {
@@ -53,20 +46,12 @@ int main(int argc, char*argv[]) {
   if(isatty(0)) DIE("cannot read from tty");
   if(action==0) DIE("no action: try -s, -v, -S, or -V");
   if(action=='V' && seed[0]) DIE("cannot specify --seed when using -V");
-  /* make a nice big buffer, 1M */
-  int size = 1024*1024;
+  int size = 1024*1024; /* make a nice big buffer, 1M */
   unsigned char*buf=malloc(size);
   int bytes = fread(buf, 1, size, stdin);
   if(bytes < 0) PERR("<stdin>: ");
   if(bytes < 1) DIE("file reading error.  (is it blank?)");
   switch(action){
-  case 'S':
-    ed25519_create_keypair(public_key, private_key, seed);
-    printf("pubk: "); DUMP(public_key, 32);
-    goto create_signature;
-  case 's': 
-    ed25519_create_keypair(public_key, private_key, seed);
-    goto create_signature;
   case 'V':   
     extract_lines(buf, bytes, 4, M);
     scan_line(buf+M[0]+6, M[1]-M[0]-7, public_key);
@@ -81,7 +66,9 @@ int main(int argc, char*argv[]) {
     scan_line(buf+M[1]+6, M[2]-M[1]-7, signature+32);
     buf  += M[2]; bytes = M[3] - M[2];
     goto verify_signature;
-  create_signature:
+  case 'S': case 's': 
+    ed25519_create_keypair(public_key, private_key, seed);
+    if(action=='S'){ printf("pubk: "); DUMP(public_key, 32); }
     ed25519_sign(signature, buf, bytes, public_key, private_key);
     printf("sig1: "); DUMP(signature, 32);
     printf("sig2: "); DUMP(signature+32, 32);
